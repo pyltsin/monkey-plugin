1c6d4f5e4a92d6ccbd11604240fb3f66bf9cf94e
Mikhail Pyltsin <mpyltsin@tsconsulting.com>
2022-02-16 19:42:19 +0300

    add find usages

--- src/main/kotlin/com/github/pyltsin/monkeyplugin/usages/MonkeyFindUsagesProvider.kt
+++ src/main/kotlin/com/github/pyltsin/monkeyplugin/usages/MonkeyFindUsagesProvider.kt
@@ -1,4 +1,49 @@
 package com.github.pyltsin.monkeyplugin.usages
 
-class MonkeyFindUsagesProvider {
+import com.github.pyltsin.monkeyplugin.MonkeyTypes
+import com.github.pyltsin.monkeyplugin.lexer.MonkeyFlexAdapter
+import com.github.pyltsin.monkeyplugin.psi.MonkeyNamedElement
+import com.github.pyltsin.monkeyplugin.psi.MonkeyVarDefinition
+import com.intellij.lang.HelpID
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.ElementDescriptionUtil
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
+import com.intellij.usageView.UsageViewLongNameLocation
+import com.intellij.usageView.UsageViewShortNameLocation
+
+class MonkeyFindUsagesProvider : FindUsagesProvider {
+    override fun getWordsScanner(): WordsScanner {
+        return DefaultWordsScanner(
+            MonkeyFlexAdapter(),
+            TokenSet.create(MonkeyTypes.IDENT, MonkeyTypes.VAR_DEFINITION, MonkeyTypes.SIMPLE_REF_EXPR),
+            TokenSet.EMPTY,
+            TokenSet.create(MonkeyTypes.STRING)
+        )
+    }
+
+    override fun canFindUsagesFor(psiElement: PsiElement): Boolean {
+        return psiElement is MonkeyNamedElement
+    }
+
+    override fun getHelpId(psiElement: PsiElement): String {
+        return HelpID.FIND_OTHER_USAGES
+    }
+
+    override fun getType(element: PsiElement): String {
+        return when (element) {
+            is MonkeyVarDefinition -> "variable"
+            else -> "unknown"
+        }
+    }
+
+    override fun getDescriptiveName(element: PsiElement): String {
+        return ElementDescriptionUtil.getElementDescription(element, UsageViewLongNameLocation.INSTANCE)
+    }
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+        return ElementDescriptionUtil.getElementDescription(element, UsageViewShortNameLocation.INSTANCE)
+    }
 }
